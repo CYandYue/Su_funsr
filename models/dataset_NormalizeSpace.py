3d1dcb21a4479f74d304aa416013ce185c4f9f09
CYandYUE <89253033@qq.com>
2025-02-22 08:51:08 +0800

    modify point_num in dataset_NormalizeSpace.py to adapt us_nerf's ply

--- models/dataset_NormalizeSpace.py
+++ models/dataset_NormalizeSpace.py
@@ -105,9 +105,11 @@ class Dataset:
 
         # concat sampled points with grid points 
         query_points = torch.cat([query_points, grid_f]).float()
+        
+        print(query_points.size())
 
         ## find nearest neiboring point cloud for each query point
-        POINT_NUM = 1000  # divide by batch to avoid out-of-memory
+        POINT_NUM = 100  # divide by batch to avoid out-of-memory
         query_points_nn = torch.reshape(query_points, (-1, POINT_NUM, 3))
         sample_near_tmp = []
         sample_near = []
